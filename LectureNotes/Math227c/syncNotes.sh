93c373d1b62ae2ff85539140f73249cc4e514f88
Jun Allard <jun.allard@uci.edu>
2020-04-24 12:05:01 -0700

    Fix typo in last equation of 4.2

--- LectureNotes/Math227c/syncNotes.sh
+++ LectureNotes/Math227c/syncNotes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 rsync -r ~/Google\ Drive/Math227C_2020Spring/Math227c ~/github/Math227C/LectureNotes
-cd ~/github/Math227C/;
+cd ~/github/Math227C/LectureNotes/Math227c;
 rm Icon*;
 git add .
 git commit -m 'After lecture hand notes'
