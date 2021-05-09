cdbd4811a348c2f2e5e00b077c27308de3571b28
Jun Allard <jun.allard@uci.edu>
2021-05-09 16:55:13 -0700

    first attempt at github action

--- LectureNotes/syncNotes.sh
+++ LectureNotes/syncNotes.sh
@@ -2,5 +2,6 @@
 rsync -r ~/Google\ Drive/Notability/Math227c21Sp/ ~/github/Math227C/LectureNotes
 cd ~/github/Math227C/LectureNotes;
 rm Icon*;
+cd ../
 git add .
 git commit -m 'After lecture, sync hand notes'
