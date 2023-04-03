3905f7a2972bb2488cb917252bcdb8b29f85f7f7
Jun Allard <jun.allard@uci.edu>
2023-04-03 16:00:45 -0700

    After lecture, sync hand notes

--- LectureNotes/syncNotes.sh
+++ LectureNotes/syncNotes.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-rsync -r /Volumes/GoogleDrive-118292251643994314192/My\ Drive/Notability/M227C/ ~/git/pub/Math227C/LectureNotes
+#rsync -r /Volumes/GoogleDrive-118292251643994314192/My\ Drive/Notability/M227C/ ~/git/pub/Math227C/LectureNotes
+rsync -r "/Users/jun/jallard@uci.edu - Google Drive/My Drive/Notability" "~/git/pub/Math227C/LectureNotes"
 cd ~/git/pub/Math227C/LectureNotes;
 rm Icon*;
 cd ../
